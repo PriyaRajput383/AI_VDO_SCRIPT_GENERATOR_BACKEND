22ad9069c4ce237beb7416e331c2a724a3579062
priya rajput <priyarajput3838@gmail.com>
2026-04-02 10:45:53 +0530

    change

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ const __dirname = path.dirname(__filename);
 
 // ✅ config
 dotenv.config();
-app.use(cors());
+app.use(cors({
+  origin:"https://ai-vdo-script-generator.netlify.app/"
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
